103fa3727fc92ffb9b40e85f47b1b5b5e301b4af
Nayelly <nayellypantoja2000@gmail.com>
2023-03-07 14:46:41 -0500

    Parcial terminado

--- pantoja_nayelly/src/App.jsx
+++ pantoja_nayelly/src/App.jsx
@@ -1,12 +1,36 @@
 import './App.css'
+import React, { useState } from 'react'
 import Card from './components/Card'
 
 function App() {
 
+  const [user, setUser] = useState({
+    nombre: '',
+    peliculaFavorita: ''
+})
+const [show, setShow] = useState(false)
+const [error, setError] = useState("")
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    const nombreSinEspacios = user.nombre.trim()
+    if (nombreSinEspacios.length > 3 && user.peliculaFavorita.length > 6) {
+      setShow(true)
+      setError("")
+    } else{
+      setError("Por favor chequea que la información sea correcta")
+    }
+  }
+
   return (
-    <div className="App">
-      <form></form>
-      <Card />
+    <div className='contenedor'>
+      <form onSubmit={handleSubmit}>
+        <input className= 'inputs' placeholder='Ingresa tu nombre' type="text" name="nombre" value={user.nombre} onChange={(e) => setUser({...user, nombre: e.target.value})} />
+        <input className= 'inputs' placeholder='Ingresa tu pelicula favorita' type="text" name="peliculaFavorita" value={user.peliculaFavorita} onChange={(e) => setUser({...user, peliculaFavorita: e.target.value})} />
+        <button className='boton' type="submit">Enviar</button>
+      </form>
+      {error && <p style={{color: "red"}}>{error}</p>}
+      {show  && <Card nombre={user.nombre} pelicula={user.peliculaFavorita}/>}
     </div>
   )
 }
--- pantoja_nayelly/src/components/Card.jsx
+++ pantoja_nayelly/src/components/Card.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 
-export const Card = () => {
+export const Card = ({nombre, pelicula}) => {
+  const styles = {
+    contenedor: {
+      backgroundColor: "#f2f2f2",
+      borderRadius: "5px",
+      textAlign: "center",
+      width: '500px',
+      height: '300px'
+    },
+    h3: {
+      color: "#333",
+      fontSize: "24px"
+    },
+    p: {
+      color: "#666",
+      fontSize: "18px"
+    },
+    h1: {
+      color: "white",
+      backgroundColor: "#C0FF40",
+      margin: "10px",
+      padding: "10px",
+      borderRadius: "5px",
+      fontSize: "30px"
+    }
+  };
   return (
-    <div>Card</div>
+    <div style={styles.contenedor}>
+        <h3 style={styles.h3}>¡Hola {nombre}!</h3>
+        <p style={styles.p}>Sabemos que tu pelicula favorita es: </p>
+        <h1 style={styles.h1}>{pelicula}</h1>
+    </div>
   )
 }
 export default Card
